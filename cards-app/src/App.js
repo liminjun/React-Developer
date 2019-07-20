2a4ddb901221d847e344d541b7dff636e1d8cb08
liminjun <v-leeli@hotmail.com>
2019-07-20 18:22:55 +0800

    feat:add Github Card App

---
+++ cards-app/src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './main.less';
+import { tsConstructorType } from '@babel/types';
+
+
+const testData = [{
+  name: "lee", avatar_url: "https://avatars2.githubusercontent.com/u/966009?s=460&v=4", company: "cm soft"
+},
+{ name: "mingxing", avatar_url: "https://avatars2.githubusercontent.com/u/966009?s=460&v=4", company: "知图" }
+];
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card {...profile} />)}
+
+  </div>
+)
+class Form extends React.Component {
+  state = {
+    userName: ''
+  };
+  handleSubmit = (event) => {
+
+    event.preventDefault();
+    console.log(
+      this.state.userName
+    );
+  }
+  render() {
+    return (
+      <form action="" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Github username" value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          required></input>
+        <button>Add Card</button>
+      </form>
+    )
+  }
+}
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url}></img>
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    )
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+
+    }
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <Form />
+        <CardList profiles={this.state.profiles}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
